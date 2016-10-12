80517d6221c16583351c294999d20ae8d68d2cb2
jackieluc <jaluc@ucalgary.ca>
2016-10-12 13:42:34 -0600

    update to post tweets

--- bot.js
+++ bot.js
@@ -2,18 +2,41 @@
 var Twit = require('twit');
 var config = require('./config');
 
+// create new Twit with config (consumer key, access key)
 var T = new Twit(config);
 
+// parameters for searching for tweets
 var parameters = {
-  q: 'banana since:2011-07-11',
-  count: 3
+  q: 'pun since:2014-07-11',
+  count: 5
 }
 
-function getData(err, data, response) {
+// list of tweets to send out
+var tweetList = {
+  status: 'first test from bot #coding'
+}
+
+// function that gets the tweet data and prints it to console
+function getTweet(err, data, response) {
   var tweets = data.statuses;
   for(var i = 0; i < tweets.length; i++) {
     console.log(tweets[i].text);
   }
 }
 
-T.get('search/tweets', parameters, getData);
+// function that deals with the response from tweeting, ie error handling when tweet exceeds 140 characters
+function whenTweeted(err, data, response) {
+  if (err) {
+    console.log("Error when tweeting!");
+    console.log(err);
+  }
+  else {
+      console.log("Sending tweet...");
+  }
+}
+
+// get tweets
+// T.get('search/tweets', parameters, getTweet);
+
+// posts tweets
+T.post('statuses/update', tweetList, whenTweeted);
