6d1ffd1c34ca350ce39120999b281587a2921164
jackieluc <jaluc@ucalgary.ca>
2016-10-12 13:24:37 -0600

    updated removal of consumer/access keys and added function to search tweets with keyword

--- bot.js
+++ bot.js
@@ -4,12 +4,16 @@ var config = require('./config');
 
 var T = new Twit(config);
 
+var parameters = {
+  q: 'banana since:2011-07-11',
+  count: 3
+}
 
-//
-// var T = new Twit({
-//   consumer_key:         'Joj0S4v0ps5Su4GivZQoYvZBU',
-//   consumer_secret:      'TgCIkfdurs7UdYqLs56bSR930ik2W5o5PzyxtvbJ6BpSyeVvss',
-//   access_token:         '779099965222072320-bvThonisp8zi3mp9QoUTKnRX0e1xhxQ',
-//   access_token_secret:  'cCF9gtdT5vb5uuwh139inHG8X8ZvdS9Ddg64dZsilQTA2',
-//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-// });
+function getData(err, data, response) {
+  var tweets = data.statuses;
+  for(var i = 0; i < tweets.length; i++) {
+    console.log(tweets[i].text);
+  }
+}
+
+T.get('search/tweets', parameters, getData);
