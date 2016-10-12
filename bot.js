1d7bbc58e17b34fb87ef3543a6916725a79f61d3
jackieluc <jaluc@ucalgary.ca>
2016-10-12 14:50:15 -0600

    added basic functions for replying to follows and mentions

--- bot.js
+++ bot.js
@@ -5,15 +5,44 @@ var config = require('./config');
 // create new Twit with config (consumer key, access key)
 var T = new Twit(config);
 
+// setting up a user stream (continuous stream to twitter)
+var userStream = T.stream('user');
+
+// anytime someone follows me or mentions me
+userStream.on('follow', followed);
+userStream.on('tweet', mentioned);
+
+// handles when a user follows me
+function followed(event) {
+  var name = event.source.name;
+  var twitterUsername = event.source.source_name;
+
+  // use .@ if you want to add it to Tweets timeline
+  postTweet('@' + twitterUsername + " Thanks for subscribing for puns!");
+}
+
+// how to write to json file
+// // fs = file system (node)
+// var fs = require('fs');
+//
+// // javascript string into json
+// var json = JSON.stringify(tweet, null, 2);
+// fs.writeFile("tweet.json", json);
+
+function mentioned(event) {
+  var replyTo = event.in_reply_to_screen_name;
+  var text = event.text;
+  var from = event.user.screen_name;
+
+  if (replyTo === 'alittlepunny') {
+    postTweet('@' + from + ' hmm...');
+  }
+}
+
 // parameters for searching for tweets
 var parameters = {
   q: 'pun since:2014-07-11',
   count: 5
-}
-
-// list of tweets to send out
-var tweetList = {
-  status: 'first test from bot #coding'
 }
 
 // function that gets the tweet data and prints it to console
@@ -24,19 +53,29 @@ function getTweet(err, data, response) {
   }
 }
 
-// function that deals with the response from tweeting, ie error handling when tweet exceeds 140 characters
-function whenTweeted(err, data, response) {
-  if (err) {
-    console.log("Error when tweeting!");
-    console.log(err);
-  }
-  else {
-      console.log("Sending tweet...");
-  }
-}
-
 // get tweets
 // T.get('search/tweets', parameters, getTweet);
 
 // posts tweets
-T.post('statuses/update', tweetList, whenTweeted);
+function postTweet(txt) {
+  // list of tweets to send out
+  var tweetList = {
+    status: txt
+  }
+
+  // function that deals with the response from tweeting, ie error handling when tweet exceeds 140 characters
+  function whenTweeted(err, data, response) {
+    if (err) {
+      console.log('Error when tweeting!');
+      console.log(err);
+    }
+    else {
+        console.log('Sending tweet... ' + tweetList.status);
+    }
+  }
+
+  T.post('statuses/update', tweetList, whenTweeted);
+}
+
+// tweet every hour
+// setInterval(postTweet, 1000*60*60);
