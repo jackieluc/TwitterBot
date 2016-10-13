bf1c1729875ae8b3b864567e8e7596e3b4a3bb26
jackieluc <jaluc@ucalgary.ca>
2016-10-12 22:35:09 -0600

    fix config file

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    	consumer_key : process.env.CKEY || require('./credentials.json').twitter.consumer_key,
+      consumer_key : process.env.CKEY || require('./credentials.json').twitter.consumer_key,
       consumer_secret : process.env.CSECRET || require('./credentials.json').twitter.consumer_secret,
-      access_token : process.env.ATOKEN || require('./credentials.json').twitter.acess_token,
-      access_token_secret : process.env.ATSECRET || require('./credentials.json').twitter.acess_token_secret,
+      access_token : process.env.ATOKEN || require('./credentials.json').twitter.access_token,
+      access_token_secret : process.env.ATSECRET || require('./credentials.json').twitter.access_token_secret
 }
