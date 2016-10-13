6992daa4131bea52c7db453c71517a4554848824
Jackie Luc <jackie_luc@hotmail.com>
2016-10-12 22:20:54 -0600

    Remove credentials

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key:         'VPsvutacIa6GGipmKqD0vIdRi',
-    consumer_secret:      '3j2gjAe8P5zsyaSWsnKQsZumy3bQB2qMBgWAstxb62nSLWCWbz',
-    access_token:         '779099965222072320-i4vI4DrSnpRqOh9xJiWXsAuzeNcagLo',
-    access_token_secret:  'FOw2eqpUVNVLhAo2BqegKGMyMXpp3NUhUiRxvyncDolbR',
-}
